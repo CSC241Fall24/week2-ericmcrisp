49db65badcaf3ebf15dc6c4c890eb8c0ee29a742
ericmcrisp <eric.m.crisp@outlook.com>
2024-10-09 13:22:41 -0400

    fixed problem 1, again

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -5,29 +5,29 @@ public class Q1WeirdFunction {
     public static int fRecursive(int n) {
         if (n < 3) {
            return n;
-        } else {
-           while(n >= 3) {
-              n = fRecursive(n - 1) + ( 2 * fRecursive(n - 2) ) + ( 3 * fRecursive(n - 3) );
-           }
-           return n;
-        }
-     }
+        } else { 
+            return fRecursive(n - 1) + ( 2 * fRecursive(n - 2) ) + ( 3 * fRecursive(n - 3) );
+           } 
+        } 
 
-     // a comment
   
      public static int fIterative(int n) {
       if (n < 3) {
          return n;
       }
-      int[] results = new int[n+1];
-      results[0] = 0;
-      results[1] = 1;
-      results[2] = 2;
+      int[] result = new int[n+1];
+      result[0] = 0;
+      result[1] = 1;
+      result[2] = 2;
 
       for (int i = 3; i <= n; i++) {
-         results[i] = results[i-1] + 2 * results[i-2] + 3 * results[i-3]; 
+         result[i] = result[i-1] + 2 * result[i-2] + 3 * result[i-3]; 
       }
-
-      return result;
+      return result[n];
      }
+
+   //   public static void main(String[] args) {
+   //       int x = fRecursive(3);
+   //       System.err.println(x);
+   //   }
 }
